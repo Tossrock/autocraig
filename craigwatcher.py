b5e4696358cc70d05a8bcbc6707716e69aea3aef
Kevin <tossrock@gmail.com>
2012-08-19 19:44:38 -0700

    bug fixes to email

--- craigwatcher.py
+++ craigwatcher.py
@@ -34,5 +34,5 @@ for post in post_soup.find_all("p","row"):
     msg['to']      = "Interested parties"
 
     s = smtplib.SMTP('localhost')
-    s.sendmail("Craigwatcher",["tossrock@gmail.com","jeffawang@gmail.com","adamrhine@gmail.com"],msg.as_string())
+    s.sendmail("Craigwatcher@ec2-23-20-227-25.compute-1.amazonaws.com",["tossrock@gmail.com","jeffawang@gmail.com","adamrhine@gmail.com"],msg.as_string())
     s.quit()
